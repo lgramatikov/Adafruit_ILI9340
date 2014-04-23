a03ed1c7f9dc3479cf30622660c2a5dfdbb1018d
lgramatikov <lgramatikov@yahoo.com>
2014-04-23 22:17:22 +0300

    Change SPI to DIV2 for better performance as peekay​123 advised.
    See https://community.spark.io/t/adafruit-ili9340-library-ported/4010/6 for details.

--- Adafruit_ILI9340.cpp
+++ Adafruit_ILI9340.cpp
@@ -109,10 +109,8 @@ void Adafruit_ILI9340::begin(void) {
 	dcport = &_dc;
 
 	SPI.begin();
-	//TODO, lgramatikov, core runs at 72MHz. 11 gives 6.5. But looks like Spark can do only predefined values - http://docs.spark.io/#/firmware/communication-spi 
-	//16 looks like good start.
-	SPI.setClockDivider(SPI_CLOCK_DIV16); //not quite full! speed! :)
-	//SPI.setClockDivider(11); // 85MHz / 11 = 7.6 MHz (full! speed!)
+
+	SPI.setClockDivider(SPI_CLOCK_DIV2);
 
 	SPI.setBitOrder(MSBFIRST);
 	SPI.setBitOrder(MSBFIRST);
